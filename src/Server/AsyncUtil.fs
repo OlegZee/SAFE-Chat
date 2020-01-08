f030f43672e9c56ed68e086d45b38f630b2603e7
Oleg Zee <olegzzzz@gmail.com>
2020-01-08 23:17:48 +0700

    update deps for netcore sdk 3.x

--- src/Server/AsyncUtil.fs
+++ src/Server/AsyncUtil.fs
@@ -1,19 +1,19 @@
 module AsyncUtil
 
 type AsyncResult<'T1, 'T2> = Async<Result<'T1, 'T2>>
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+
 module AsyncResult =
     let map f a = async {
         let! result = a
         return result |> Result.map f }
     let bind f a = async {
         match! a with
-        | Result.Ok r -> return! f r
-        | Result.Error r -> return Result.Error r }
+        | Ok r -> return! f r
+        | Error r -> return Error r }
     let liftMap f r = async {
         match r with
-        | Result.Ok cch ->  let! r = f cch in return Result.Ok r
-        | Result.Error x -> return Result.Error x
+        | Ok cch ->  let! r = f cch in return Ok r
+        | Error x -> return Error x
     }
 
 module Async =
--- src/Server/Prelude.fs
+++ src/Server/Prelude.fs
@@ -2,5 +2,6 @@
 module Prelude
 
 let (><) f a b = f b a
+let (^) f a = f a
 let inline (|OtherwiseFail|) _ = failwith "no choice"
 let inline (|OtherwiseFailErr|) message _ = failwith message

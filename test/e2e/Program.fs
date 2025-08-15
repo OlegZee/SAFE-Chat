20b4bcf7f5c0c65ee8c76bcd84ef4b6f748825d8
olegz <oleg.zaimkin@developertools.com>
2025-08-15 12:31:23 +0200

    Refactor CI workflow: enhance pull request triggers and ignore paths for better clarity and efficiency

--- test/e2e/Program.fs
+++ test/e2e/Program.fs
@@ -1,9 +1,7 @@
 ﻿//these are similar to C# using statements
-open canopy
 open canopy.classic
 open canopy.runner.classic
 open canopy.configuration
-open System
 open System.IO
 
 open WebDriverManager
@@ -16,10 +14,10 @@ let main args =
     let chromeDriverPath = (DriverManager ()).SetUpDriver(new ChromeConfig(), Helpers.VersionResolveStrategy.MatchingBrowser)
     
     // Tell Canopy where to find ChromeDriver
-    chromeDir <- Path.GetDirectoryName(chromeDriverPath)
+    chromeDir <- Path.GetDirectoryName chromeDriverPath
 
     // Configure for CI environment - Canopy 2.1.0 uses environment variable for headless
-    if System.Environment.GetEnvironmentVariable("CI") = "true" then
+    if System.Environment.GetEnvironmentVariable "CI" = "true" then
         System.Environment.SetEnvironmentVariable("CANOPY_HEADLESS", "true")
 
     start chrome
